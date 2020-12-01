8fd6a482b5598afdcb39f43e042de012bbdee6f9
Seokjin Yoon <sjy5386@gmail.com>
2020-12-01 12:42:55 +0900

    Add when

--- src/com/sysbot32/netptalk/server/ChatConnection.kt
+++ src/com/sysbot32/netptalk/server/ChatConnection.kt
@@ -18,30 +18,33 @@ class ChatConnection(val connection: Connection, private val chatServer: ChatSer
         while (true) {
             val received: String = connection.read() ?: break
             println(received)
-            val jsonObject: JSONObject = JSONObject(received)
-            val type: String = jsonObject.getString("type")
-            if (type == "chat") {
-                val chatMessage: ChatMessage = ChatMessage(jsonObject)
-                run {
-                    chatServer.chatRooms.forEach {
-                        if (it.title == chatMessage.chatRoom) {
-                            it.chatMessages.add(chatMessage)
-                            return@run
+            val jsonObject = JSONObject(received)
+            when (jsonObject.getString("type")) {
+                "chat" -> {
+                    val chatMessage = ChatMessage(jsonObject)
+                    run {
+                        chatServer.chatRooms.forEach {
+                            if (it.title == chatMessage.chatRoom) {
+                                it.chatMessages.add(chatMessage)
+                                return@run
+                            }
                         }
                     }
-                }
-                chatServer.write(received)
-            } else if (type == "chatRoom") {
-                val chatRoom: ChatRoom = ChatRoom(jsonObject.getString("title"))
-                if (jsonObject.getString("action") == "add") {
-                    chatServer.chatRooms.add(chatRoom)
                     chatServer.write(received)
                 }
-            } else if (type == "login") {
-                username = jsonObject.getString("username")
-                println("${username}님이 로그인했습니다.")
-                chatServer.chatRooms.forEach {
-                    connection.write(it.toJSONObject().put("action", "add").toString())
+                "chatRoom" -> {
+                    val chatRoom = ChatRoom(jsonObject.getString("title"))
+                    if (jsonObject.getString("action") == "add") {
+                        chatServer.chatRooms.add(chatRoom)
+                        chatServer.write(received)
+                    }
+                }
+                "login" -> {
+                    username = jsonObject.getString("username")
+                    println("${username}님이 로그인했습니다.")
+                    chatServer.chatRooms.forEach {
+                        connection.write(it.toJSONObject().put("action", "add").toString())
+                    }
                 }
             }
         }
